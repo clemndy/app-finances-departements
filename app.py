5dacc7ce2a40ab6285e1de54290d3cdf9b04a36e
clemndy <clement.naudy@edu.devinci.fr>
2026-06-01 11:45:49 +0200

    test
    Refactor financial analysis tool by reintroducing necessary imports and adjusting page configuration. Update data loading and visualization functions to enhance user experience.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import streamlit as st
 
 # CONFIGURATION DE LA PAGE WEB
-st.set_page_config(page_title="Analyse Financière Départementale", page_icon="📊")
+st.set_page_config(page_title="AAAAAAAnalyse Financière Départementale", page_icon="📊")
 
 # --- 2. CHARGEMENT DES DONNÉES ---
 @st.cache_data
