55d6e966cefda3a0b021aa8749e48e522db790d8
clemndy <clement.naudy@edu.devinci.fr>
2026-06-23 11:11:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -254,7 +254,7 @@ def analyser_un_departement(df_arg, code_dep, intervalle_annees, indicateurs, pa
                     sns.lineplot(data=pivot, x="Exercice", y=indic_temp, marker="o", label=indic_temp, ax=ax2, linewidth=3)
                     
             else:    # Si on a vraiment rien à afficher, que ce soit parce qu'on a jamais trouvé la colonne ou que les données n'étaient pas normalisables
-                 if "(€/hab)" in indic_temp and any(m in indic_temp for m in ["Capacité", "Poids"]):
+                if "(€/hab)" in indic_temp and any(m in indic_temp for m in ["Capacité", "Poids"]):
                     label_txt = f"⚠️ {indic_temp.replace(' (€/hab)', '')} (Non normalisable)"
                 else:
                     label_txt = f"⚠️ {indic_temp} indisponible"
